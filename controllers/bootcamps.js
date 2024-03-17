b682e62037e01be52d85015e2307c336b871aa41
NguyenHHKiet <1851050073kiet@ou.edu.vn>
2024-03-17 12:25:08 +0700

    middleware error

--- controllers/bootcamps.js
+++ controllers/bootcamps.js
@@ -30,7 +30,8 @@ exports.getBootcamp = async (req, res, next) => {
 
         res.status(200).json({ success: true, data: bootcamp });
     } catch (err) {
-        res.status(400).json({ success: false });
+        // res.status(400).json({ success: false });
+        next(err);
     }
 };
 
