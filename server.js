9f89df00e1b39fdea65ef3878edab4a0993f0b45
NguyenHHKiet <1851050073kiet@ou.edu.vn>
2024-03-20 19:52:24 +0700

    setImmediate error

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const dotenv = require("dotenv");
 const morgan = require("morgan");
 const colors = require("colors");
 const errorHandler = require("./middleware/error");
+const ErrorResponse = require("./utils/errorResponse");
 const connectDB = require("./config/db");
 
 // Load environment variables
@@ -29,6 +30,13 @@ if (process.env.NODE_ENV === "development") {
 app.use("/api/v1/bootcamps", bootcamps);
 app.use("/api/v1/courses", courses);
 
+// next handler error if something goes wrong ---------------------------
+app.use((req, res, next) => {
+    // Error goes via `next()` method now
+    setImmediate(() => {
+        next(new ErrorResponse("Something went wrong!!!", 500));
+    });
+});
 app.use(errorHandler);
 
 const PORT = process.env.PORT || 8080;
