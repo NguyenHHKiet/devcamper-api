5b55335a6876649b6e772aaada0ddbc71d429167
NguyenHHKiet <1851050073kiet@ou.edu.vn>
2024-04-04 18:25:46 +0700

    register and login

--- controllers/auth.js
+++ controllers/auth.js
@@ -4,9 +4,47 @@ const ErrorResponse = require("../utils/errorResponse");
 const User = require("../models/User");
 
 // @desc    Register user
-// @route   Get /api/v1/auth/register
+// @route   Post /api/v1/auth/register
 // @access  Public
 exports.register = asyncHandler(async (req, res, next) => {
-    res.status(200).json({success: true});
+    const { name, email, password, role } = req.body;
 
+    // Create user
+    const user = await User.create({ name, email, password, role });
+
+    // Create token
+    const token = user.getSignedJwtToken();
+
+    res.status(200).json({ success: true, token });
+});
+
+// @desc    Login user
+// @route   Post /api/v1/auth/login
+// @access  Public
+exports.login = asyncHandler(async (req, res, next) => {
+    const { email, password } = req.body;
+
+    // Validate email and password
+    if (!email || !password) {
+        return next(new ErrorResponse("Invalid email or password", 400));
+    }
+
+    // Check for user
+    const user = await User.findOne({ email }).select("+password");
+
+    if (!user) {
+        return next(new ErrorResponse("Invalid credentials", 401));
+    }
+
+    // Check if password matches
+    const isMatch = await user.matchPassword(password);
+
+    if (!isMatch) {
+        return next(new ErrorResponse("Invalid credentials", 401));
+    }
+
+    // Create token
+    const token = user.getSignedJwtToken();
+
+    res.status(200).json({ success: true, token });
 });
--- models/User.js
+++ models/User.js
@@ -36,4 +36,22 @@ const UserSchema = new mongoose.Schema({
     },
 });
 
+// Encrypt the password using bcrypt
+UserSchema.pre("save", async function (next) {
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+});
+
+// Sign JWT and return
+UserSchema.methods.getSignedJwtToken = function () {
+    return jwt.sign({ id: this._id }, process.env.JWT_SECRET, {
+        expiresIn: process.env.JWT_EXPIRE,
+    });
+};
+
+// Match user entered password to hashed password in database
+UserSchema.methods.matchPassword = async function (enteredPassword) {
+    return await bcrypt.compare(enteredPassword, this.password);
+};
+
 module.exports = mongoose.model("User", UserSchema);
